1ccc0a7ffbd9abedaa3fd6a6c51b936d589a9e8b
mesode <akwe.mesode@gmail.com>
2021-05-25 16:02:11 +0100

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 
 import React, { FC, useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  FlatList, 
+  TouchableOpacity } from 'react-native';
 import Input from './input';
 import { Fruit, Fruits } from './data';
 import Item from './listitem';
@@ -8,16 +13,25 @@ import Item from './listitem';
 const App: FC = () => {
   const [ searchquery, setSearchQuery ] = useState<string>("");
   const [ fruits, setFruits ] = useState<Fruit[] | null>(null);
+  const [inputShown, setInputShown] = useState<boolean>(false);
+  const [newFruit, setNewFruit] = useState<Fruit | null>(null);
 
   useEffect(() => {
     (() => {
-      setFruits(Fruits);
+      setFruits(Fruits.sort((a: Fruit, b: Fruit) => {
+        return a.price > b.price ? 1 : b.price > a. price ? -1 : 0;
+      }));
     })();
   }, []);
   
   const handleSearch = (text: string) => {
     const fruits: Fruit[] = Fruits.filter(fruit => fruit.name.includes(text));
     setFruits(fruits);
+  };
+
+  const handleAdd = () => {
+    if (newFruit !== null && fruits !== null) setFruits([...fruits, newFruit]);
+    else if(newFruit !== null && fruits == null) setFruits([newFruit]);
   };
 
   return (
@@ -37,6 +51,57 @@ const App: FC = () => {
             />
           )} 
         />
+        <View>
+        <TouchableOpacity 
+            style={{
+              alignSelf: 'center',
+              backgroundColor: 'rgba(81,135,200,1)',
+              padding: 10,
+              paddingHorizontal: 20,
+              borderRadius: 6,
+              marginVertical: 10,
+              display: inputShown == false ? 'flex' : 'none',
+            }}
+            onPress={() => setInputShown(true)}
+            >
+              <Text style={{ color: 'white', fontWeight: '600', }}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{display: inputShown == true? 'flex' : 'none'}}>
+          <Input 
+            icon="ios-add-circle-outline" 
+            placeholder="Fruit Name" 
+            onChangeText={(text) => { 
+            if(newFruit !== null){
+              setNewFruit({...newFruit, name: text})
+            } else{
+              setNewFruit({ id: Date.now(), name: text, price: 0})
+            }
+          }} />
+          <Input 
+            icon="ios-add-circle-outline" 
+            placeholder="Fruit Price" 
+            onChangeText={(text) => { 
+              if(newFruit !== null){
+                setNewFruit({...newFruit, price: +text})
+              } else{
+                setNewFruit({ id: Date.now(), name: '', price: +text})
+              }
+            }} />
+          <TouchableOpacity 
+            style={{
+              alignSelf: 'center',
+              backgroundColor: 'rgba(81,135,200,1)',
+              padding: 10,
+              paddingHorizontal: 20,
+              borderRadius: 6,
+              marginVertical: 10,
+            }}
+            onPress = {handleAdd}
+            >
+              <Text style={{ color: 'white', fontWeight: '600', }}>Add</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
